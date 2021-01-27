309d026df463a8ebd38b55b9cb722055d99519a4
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-01-27 16:14:44 +0300

    Remove file-changed warn

--- dumper.sh
+++ dumper.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 cd ${DATA_FOLDER}
 
-tar -zcf backup.tar.gz . 
+tar -zcf --warning=no-file-changed   backup.tar.gz . 
 
 _BACKUP_FILENAME=backup_$(date '+%Y-%m-%d_%H:%M').tar.gz
 mv backup.tar.gz ${BACKUPS_FOLDER}/${_BACKUP_FILENAME}
